8a5170bcf94e9ac4316c5e6d6296d0df0b9a7280
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-01-16 19:33:05 +0100

    Add new tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 11:24:15 by jkettani          #+#    #+#             */
-/*   Updated: 2019/01/15 18:39:13 by jkettani         ###   ########.fr       */
+/*   Updated: 2019/01/16 14:42:25 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,25 @@ void	test_char(char *desc, char *fmt, char *test_str, char test)
 	printf("\n");
 }
 
+void	test_unsigned_char(char *desc, char *fmt, char *test_str, unsigned char test)
+{
+	test_desc(desc, fmt, test_str);
+	printf("|" YELLOW);
+	printf(fmt, test);
+	printf(NC "|");
+	printf("\n");
+}
+
 void	test_int(char *desc, char *fmt, char *test_str, int test)
+{
+	test_desc(desc, fmt, test_str);
+	printf("|" YELLOW);
+	printf(fmt, test);
+	printf(NC "|");
+	printf("\n");
+}
+
+void	test_unsigned_int(char *desc, char *fmt, char *test_str, unsigned int test)
 {
 	test_desc(desc, fmt, test_str);
 	printf("|" YELLOW);
@@ -69,6 +87,15 @@ void	test_short(char *desc, char *fmt, char *test_str, short test)
 }
 
 void	test_long(char *desc, char *fmt, char *test_str, long test)
+{
+	test_desc(desc, fmt, test_str);
+	printf("|" YELLOW);
+	printf(fmt, test);
+	printf(NC "|");
+	printf("\n");
+}
+
+void	test_unsigned_long(char *desc, char *fmt, char *test_str, unsigned long test)
 {
 	test_desc(desc, fmt, test_str);
 	printf("|" YELLOW);
@@ -114,12 +141,17 @@ void	test_type_int(void)
 	print_h1("TYPE: INT (%d | %i)");
 	print_h2("=> Valid format:");
 	test_int("Zero", "%d", "0", 0);
-	test_int("Zero with 0 precision", "%.d", "0", 0);
+	test_int("Zero with .0 precision", "%.d", "0", 0);
+	test_int("Zero with .3 precision", "%.3d", "0", 0);
 	test_int("Random int", "%d", "42", 42);
 	test_int("Int max", "%d", "2147483647", INT_MAX);
 	test_int("Int max + 1", "%d", "2147483648", INT_MAX + 1);
 	test_int("Int min", "%d", "-2147483648", INT_MIN);
 	test_int("Unsigned int max", "%d", "4294967295", UINT_MAX);
+	test_int("Random int with precision .1", "%.1d", "42", 42);
+	test_int("Random int with precision .4", "%.1d", "42", 42);
+	test_int("Random int with 3 width and .5 precision -- precision > width", "%3.5d", "42", 42);
+	test_int("Random int with 5 width and .3 precision -- precision < width", "%5.3d", "42", 42);
 	test_int("Random int filled with 0", "%0d", "42", 42);
 	test_int("Random int with 5 spaces filled with 0", "%05d", "42", 42);
 	test_int("Random int with 5 spaces filled with 0 -- multiple 0", "%00005d", "42", 42);
@@ -137,10 +169,10 @@ void	test_type_int(void)
 	test_int("Random int left aligned with 5 spaces", "%-5d", "42", 42);
 	test_int("Random int left aligned with 5 spaces -- multiple - signs", "%------5d", "42", 42);
 	test_int("Random int left aligned with 5 spaces and + sign", "%-+5d", "42", 42);
-	test_int("Random int left aligned with 5 spaces and + sign -- multiple - and + signs", "%---++++5d", "42", 42);
+	test_int("Random int left aligned with 5 spaces and + sign -- multiple - and + signs", "%---++++---5d", "42", 42);
 	test_int("Random int left aligned with 5 spaces and space instead of + sign", "%- 5d", "42", 42);
 	test_int("Random int left aligned with 5 spaces and space instead of + sign -- reverse order", "% -5d", "42", 42);
-	test_int("Random int left aligned with 5 spaces and space instead of + sign -- multiple - and spaces", "%----    5d", "42", 42);
+	test_int("Random int left aligned with 5 spaces and space instead of + sign -- multiple - and spaces", "%----    ---5d", "42", 42);
 	test_int("Random int left aligned with 1 space", "%-1d", "42", 42);
 	test_char("Random char with hh length modifier", "%hhd", "298", (char)42);
 	test_char("Random char with hh length modifier", "%hhd", "298", (char)298);
@@ -178,6 +210,62 @@ void	test_type_octal(void)
 	test_long_long("Long long max with L length modifier", "%Lo", "9223372036854775807", LLONG_MAX);
 }
 
+void	test_type_unsigned_int(void)
+{
+	print_h1("TYPE: UNSIGNED INT (%u)");
+	print_h2("=> Valid format:");
+	test_unsigned_int("Random unsigned int", "%u", "42u", 42u);
+	test_unsigned_int("Int max + 1", "%u", "2147483648", INT_MAX + 1);
+	test_unsigned_int("Unsigned int max", "%u", "4294967295", UINT_MAX);
+	test_unsigned_int("Unsigned int max + 1", "%u", "4294967296", UINT_MAX + 1);
+	test_unsigned_int("Negative number", "%u", "-1", -1);
+	test_unsigned_int("Random unsigned int with 5 width", "%5u", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and - flag", "%-5u", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and .3 precision", "%5.3u", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and .3 precision and - flag", "%-5.3u", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 3 width and .5 precision", "%3.5u", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 3 width and .5 precision", "%3.5u", "42u", 42u);
+	test_unsigned_char("Unsigned char max with hh length modifier", "%hhu", "255", (unsigned char)UCHAR_MAX);
+	test_unsigned_char("Unsigned char max + 1  with hh length modifier", "%hhu", "256", (unsigned char)(UCHAR_MAX + 1));
+	test_unsigned_long("Unsigned long max with l length modifier", "%lu", "18446744073709551615", (unsigned long)ULONG_MAX);
+}
+
+void	test_type_hexadecimal_x(void)
+{
+	print_h1("TYPE: HEXADECIMAL (%x)");
+	print_h2("=> Valid format:");
+	test_unsigned_int("Random unsigned int", "%x", "42u", 42u);
+	test_unsigned_int("Zero", "%x", "0u", 0u);
+	test_unsigned_int("Zero with .0 precision", "%.x", "0u", 0u);
+	test_unsigned_int("Zero with .3 precision", "%.3x", "0u", 0u);
+	test_unsigned_int("Unsigned int max", "%x", "4294967295u", UINT_MAX);
+	test_unsigned_int("Unsigned int max + 1", "%x", "4294967296u", UINT_MAX + 1);
+	test_unsigned_int("Random unsigned int with 5 width", "%5x", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and # flag", "%#5x", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and # flag and .3 precision", "%#5.3x", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 10 width and # and 0 flags", "%#010x", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and - flag", "%-5x", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and 0 flag", "%05x", "42u", 42u);
+}
+
+void	test_type_hexadecimal_X(void)
+{
+	print_h1("TYPE: HEXADECIMAL (%X)");
+	print_h2("=> Valid format:");
+	test_unsigned_int("Random unsigned int", "%X", "42u", 42u);
+	test_unsigned_int("Zero", "%X", "0u", 0u);
+	test_unsigned_int("Zero with .0 precision", "%.X", "0u", 0u);
+	test_unsigned_int("Zero with .3 precision", "%.3X", "0u", 0u);
+	test_unsigned_int("Unsigned int max", "%X", "4294967295u", UINT_MAX);
+	test_unsigned_int("Unsigned int max + 1", "%X", "4294967296u", UINT_MAX + 1);
+	test_unsigned_int("Random unsigned int with 5 width", "%5X", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and # flag", "%#5X", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and # flag and .3 precision", "%#5.3X", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 10 width and # and 0 flags", "%#010X", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and - flag", "%-5X", "42u", 42u);
+	test_unsigned_int("Random unsigned int with 5 width and 0 flag", "%05X", "42u", 42u);
+}
+
 int		main(int ac, char **av)
 {
 	(void)ac;
@@ -189,7 +277,12 @@ int		main(int ac, char **av)
 	//OCTAL
 	test_type_octal();
 	//UNSIGNED INT
-	printf("|%.5d|\n", 42);
+	test_type_unsigned_int();
+	//HEXADECIMAL x
+	test_type_hexadecimal_x();
+	//HEXADECIMAL X
+	test_type_hexadecimal_X();
 	printf("\n");
+	printf("|%#010x|\n", 42);
 	return (0);
 }
